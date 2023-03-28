bc3ced2e437d87321fd6caafd7c9f6742f851f76
xxx <xxx@web.de>
2023-03-28 22:24:47 +0200

    c04-branch02-del injectors

--- src/pz/EmailMessageServiceInjectorImp.java
+++
@@ -1,7 +0,0 @@
-package pz;
-public class EmailMessageServiceInjectorImp implements MessageServiceInjector {
-    @Override
-    public MessageServiceConsumer getMessageServiceConsumer() {
-        return new MessageServiceConsumerImp(new EmailServiceImp());
-    }
-}
